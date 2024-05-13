6035eeb41b105087edb35844e824c48cff2764ef
antoniomayk <antoniomayk.g@gmail.com>
2024-05-13 09:46:30 -0300

    00003

--- src/main/scala/Solution00003.scala
+++ src/main/scala/Solution00003.scala
@@ -1,13 +1,14 @@
-import scala.annotation.tailrec
-
 object Solution00003:
-  def lengthOfLongestSubstring(s: String): Int = processSubstring(s, Set.empty)
-
-  @tailrec
-  def processSubstring(s: String, set: Set[Char], left: Int = 0, longest: Int = 0, idx: Int = 0): Int =
-    (idx == s.length) match
-      case true => longest
-      case false =>
-        set(s(idx)) match
-          case true  => processSubstring(s, set - s(left), left + 1, longest, idx)
-          case false => processSubstring(s, set + s(idx), left, longest.max(idx - left + 1), idx + 1)
+  def lengthOfLongestSubstring(
+      s: String,
+      chars: Set[Char] = Set.empty,
+      l: Int = 0,
+      r: Int = 0,
+      maxLen: Int = 0
+  ): Int =
+    if (r == s.length)
+      maxLen
+    else if (chars(s(r)))
+      lengthOfLongestSubstring(s, chars - s(l), r, l + 1, maxLen)
+    else
+      lengthOfLongestSubstring(s, chars + s(r), l, r + 1, maxLen max (r - l + 1))
