9a2710d33208c61e18d20c4c0c4d50f698bb0df2
antoniomayk <antoniomayk.g@gmail.com>
2024-05-13 09:12:31 -0300

    00006

--- src/main/scala/Solution00006.scala
+++ src/main/scala/Solution00006.scala
@@ -3,12 +3,13 @@ object Solution00006:
     numRows match
       case 1 => s
       case _ =>
-        s.foldLeft(List.fill(numRows)(List.empty), 0, true)(processCharacterState)._1.map(_.mkString).mkString
+        s.foldLeft(List.fill(numRows)(List.empty[Char]), 0, true)({ (t, c) =>
+          (t, c) match
+            case ((m, r, true), c) => processCharacter(m, r, c, r + 1 < m.size)
+            case ((m, r, _), c)    => processCharacter(m, r, c, r - 1 < 0)
+        })._1
+          .map(_.mkString)
+          .mkString
 
-  def processCharacterState(st: (List[List[Char]], Int, Boolean), c: Char): (List[List[Char]], Int, Boolean) =
-    (st, c) match
-      case ((m, r, true), c) => processCharacter(m, r, c, r + 1 < m.size)
-      case ((m, r, _), c)    => processCharacter(m, r, c, r - 1 < 0)
-
-  def processCharacter(m: List[List[Char]], r: Int, c: Char, md: Boolean): (List[List[Char]], Int, Boolean) =
-    (m.updated(r, m(r) :+ c), if (md) r + 1 else r - 1, md)
+  def processCharacter(m: List[List[Char]], r: Int, c: Char, b: Boolean): (List[List[Char]], Int, Boolean) =
+    (m.updated(r, m(r) :+ c), if (b) r + 1 else r - 1, b)
