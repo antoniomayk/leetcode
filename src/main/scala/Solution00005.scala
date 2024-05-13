5c4049f17e22495a01b6297f1bd0075ce0b749d4
antoniomayk <antoniomayk.g@gmail.com>
2024-05-13 09:26:59 -0300

    00005

--- src/main/scala/Solution00005.scala
+++ src/main/scala/Solution00005.scala
@@ -1,26 +1,15 @@
 object Solution00005:
   def longestPalindrome(s: String): String =
-    s.indices.foldLeft((s, ""))(findLongestPalindromeAt)._2
+    s.indices
+      .foldLeft((s, ""))({ (t, idx) =>
+        val p1 = expand(t._1, idx, idx, t._2)
+        val p2 = expand(t._1, idx, idx + 1, t._2)
+        val lp = Seq(p1, p2, t._2).maxBy(_.size)
+        (t._1, lp)
+      })
+      ._2
 
-  def findLongestPalindromeAt(t: (String, String), idx: Int): (String, String) =
-    (
-      t._1,
-      findLongestPalindromeBetween(
-        expandAroundCenter(t._1, idx, idx, t._2),
-        expandAroundCenter(t._1, idx, idx + 1, t._2),
-        t._2
-      )
-    )
-
-  def findLongestPalindromeBetween(pl1: String, pl2: String, lpl: String): String =
-    val longestestPalindrome = if (pl1.size > pl2.size) pl1 else pl2
-    if (longestestPalindrome.size > lpl.size) longestestPalindrome else lpl
-
-  def expandAroundCenter(s: String, l: Int, r: Int, p: String): String =
-    (l >= 0 && r < s.size && s(l) == s(r)) match
-      case true =>
-        if (p.size < (r + 1) - l)
-          expandAroundCenter(s, l - 1, r + 1, s.slice(l, (r + 1)))
-        else
-          expandAroundCenter(s, l - 1, r + 1, p)
-      case _ => p
+  def expand(s: String, l: Int, r: Int, lp: String): String =
+    if (l >= 0 && r < s.size && s(l) == s(r))
+      expand(s, l - 1, r + 1, Seq(lp, s.slice(l, r + 1)).maxBy(_.size))
+    else lp
